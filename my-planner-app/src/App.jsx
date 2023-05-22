9f558d0ffa397c3ef391c07ba53aee47d7bb6de1
Guilherme Baia Venâncio <guilhermebaiav@gmail.com>
2023-05-21 21:31:52 -0300

    colors and flexibility to the app

--- my-planner-app/src/App.jsx
+++ my-planner-app/src/App.jsx
@@ -3,16 +3,36 @@ import DayContainer from './components/DayContainer'
 
 function App() {
   return (
-    <div className="w-screen h-screen flex">
-      <DayContainer day="Segunda"/>
-      <DayContainer day="Terça"/>
-      <DayContainer day="Quarta"/>
-      <DayContainer day="Quinta"/>
-      <DayContainer day="Sexta"/>
-      <DayContainer day="Sábado"/>
-      <DayContainer day="Domingo"/>
+    <div className="w-screen h-screen flex flex-col flex-wrap">
+      <Container>
+        <DayContainer day="Segunda" color="bg-red-200" btnColor="bg-red-400"/>
+        <DayContainer day="Terça"  color="bg-yellow-200" btnColor="bg-yellow-400"/>
+        <DayContainer day="Quarta" color="bg-teal-200" btnColor="bg-teal-400"/>
+        <DayContainer day="Quinta" color="bg-indigo-200" btnColor="bg-indigo-400"/>
+      </Container>
+      <Container>
+        <DayContainer day="Sexta" color="bg-lime-200" btnColor="bg-lime-400"/>
+        <DayContainer day="Sábado" color="bg-purple-200" btnColor="bg-purple-400"/>
+        <DayContainer day="Domingo" color="bg-orange-200" btnColor="bg-orange-400"/>
+        <DayContainer day="Anotações" color="bg-fuchsia-200" btnColor="bg-fuchsia-400"/>
+
+      </Container>
+      
+
+    
     </div>
   )
 }
 
 export default App
+
+
+
+
+function Container({children}){
+  return (
+    <div className='flex flex-row flex-auto min-w-500'>
+      {children}
+    </div>
+  );
+}
--- my-planner-app/src/components/DayContainer.jsx
+++ my-planner-app/src/components/DayContainer.jsx
@@ -1,14 +1,17 @@
-export default function DayContainer({day}){
+export default function DayContainer({day, color, btnColor}){
+
+    let notes = ['Entregar tarefa', 'Ir na academia', 'Terminar relatório'];
+    let notesArray = notes.map(note => <Note title={note} key={crypto.randomUUID()} />);
+
+
     return (
         <>
-        <div className="bg-red-300 h-full flex-1 flex flex-col items-center p-3">
+        <div className={`${color} h-full flex-1 flex flex-col items-center p-3`}>
             <p className="text-xl">{day.toUpperCase()}</p>
             <div className="bg-amber-50 w-full h-full rounded-md divide-y divide-amber-950">
-                <Note/>
-                <Note/>
-                <Note/>
+                {notesArray}
             </div>
-            <button className="bg-red-400 w-full rounded-md mt-3 p-3 text-black/50 font-bold hover:bg-red-500">+ Nova Nota</button>
+            <button className={`${btnColor} w-full rounded-md mt-3 p-3 text-black/50 font-bold opacity-50 hover:opacity-100`}>+ Nova Nota</button>
         </div>
             
         </>
@@ -17,10 +20,10 @@ export default function DayContainer({day}){
 }
 
 
-function Note(){
+function Note({title}){
     return(
         <div className="p-3 hover:bg-amber-100 cursor-pointer">
-            <p>Entregar atividade</p>
+            <p>{title}</p>
         </div>
     );
 }
