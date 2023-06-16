32b69e255b8ec683965fb3bc7dfc1b44b044c17d
Guilherme Baia Venâncio <guilhermebaiav@gmail.com>
2023-06-15 21:28:55 -0300

    added landing page
    + logic to switch pages

--- my-planner-app/src/App.jsx
+++ my-planner-app/src/App.jsx
@@ -1,25 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 import DayContainer from './components/DayContainer'
+import LandingPage from './components/landingPage';
 
 function App() {
+
+  const [onApp, setOnApp]=useState(false);
+
   return (
-    <div className="w-screen h-screen flex flex-col flex-wrap">
-      <Container>
-        <DayContainer day="Segunda" color="bg-red-200" btnColor="bg-red-400"/>
-        <DayContainer day="Terça"  color="bg-yellow-200" btnColor="bg-yellow-400"/>
-        <DayContainer day="Quarta" color="bg-teal-200" btnColor="bg-teal-400"/>
-        <DayContainer day="Quinta" color="bg-indigo-200" btnColor="bg-indigo-400"/>
-      </Container>
-      <Container>
-        <DayContainer day="Sexta" color="bg-lime-200" btnColor="bg-lime-400"/>
-        <DayContainer day="Sábado" color="bg-purple-200" btnColor="bg-purple-400"/>
-        <DayContainer day="Domingo" color="bg-orange-200" btnColor="bg-orange-400"/>
-        <DayContainer day="Anotações" color="bg-fuchsia-200" btnColor="bg-fuchsia-400"/>
-
-      </Container>
-
-    
-    </div>
+    <>
+    {onApp?<AppPage/>:<LandingPage setPage={setOnApp}/>}
+    </>
   )
 }
 
@@ -32,6 +23,27 @@ function Container({children}){
   return (
     <div className='flex flex-row flex-auto h-1/2'>
       {children}
+    </div>
+  );
+}
+
+function AppPage(){
+  return(
+
+    <div className="w-screen h-screen flex flex-col flex-wrap">
+    <Container>
+      <DayContainer day="Segunda" color="bg-red-200" btnColor="bg-red-400"/>
+      <DayContainer day="Terça"  color="bg-yellow-200" btnColor="bg-yellow-400"/>
+      <DayContainer day="Quarta" color="bg-teal-200" btnColor="bg-teal-400"/>
+      <DayContainer day="Quinta" color="bg-indigo-200" btnColor="bg-indigo-400"/>
+    </Container>
+    <Container>
+      <DayContainer day="Sexta" color="bg-lime-200" btnColor="bg-lime-400"/>
+      <DayContainer day="Sábado" color="bg-purple-200" btnColor="bg-purple-400"/>
+      <DayContainer day="Domingo" color="bg-orange-200" btnColor="bg-orange-400"/>
+      <DayContainer day="Anotações" color="bg-fuchsia-200" btnColor="bg-fuchsia-400"/>
+
+    </Container>
     </div>
   );
 }
--- my-planner-app/src/components/landingPage.jsx
+++ my-planner-app/src/components/landingPage.jsx
@@ -1,8 +1,16 @@
 
-export default function landingPage(){
+export default function LandingPage({setPage}){
     return (
-       <>
-       <header><p>myPlanner</p></header>
-       </>
-    );
+       <div className="w-sreen h-screen flex items-center flex-col bg-gradient-to-tr from-blue-900 to-indigo-950">
+       <header className=" flex w-screen bg-gray-700/30 px-8 py-4 border-blue-200/10 border-b justify-between items-center"><p className="text-white text-2xl font-bold w-fit">myPlanner</p>
+       <p className=" w-fit text-white/50">por BaiaGui</p></header>
+
+       <div className="flex flex-col items-center content-center mt-60 w-1/2">
+       <h1 className="text-white text-6xl mb-4 text-center font-bold">Sua organização semanal de maneira fácil!</h1>
+        <h2 className="text-white/50 text-center">Introduzindo o myPlanner: seu companheiro semanal! Com design intuitivo e funcional, ele organiza suas tarefas e compromissos, permitindo que você alcance seus objetivos com facilidade. Otimize seu tempo e melhore sua produtividade com o myPlanner, sua ferramenta ideal para uma semana bem planejada..Clique no botão abaixo e descubra!</h2>
+       <button onClick={()=>{setPage(true)}} className="bg-amber-500 px-6 py-4 my-20 text-white rounded-md shadow-xl shadow-amber-300/10 hover:scale-105 transition-all">Começar</button>
+       </div>
+       
+       </div>);
+
 }
